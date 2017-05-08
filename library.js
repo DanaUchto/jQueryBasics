be0f5f635b6ce68edcec940e0e5040052709872f
Dana Uchto <danauchto@gmail.com>
2017-05-08 17:25:15 -0400

    added more animation to the object

--- library.js
+++ library.js
@@ -4,11 +4,35 @@ $(document).ready(function() {
 		width: "800px", 
 		height: "800px",
 		opacity: 0.8
-	}, 6000);
+	}, 6000,function() {
+		$(".rectangle").animate({
+			width: "200px",
+			height: "100px",
+			opacity: 0.3
+		},5000, function() {
+			$("p").css("color", "purple");
+
+		});
+
+	});
+
+	
 
 
-	$("h1").css("font-family", "Arial");
+	$("h1").css("font-family", "Comic Sans MS");
 	$("p").css("color", "darkgreen");
+
+
+	$(".anitext").click(function(){
+		$("p").css("font-family", "Comic Sans MS")
+
+	});
+
+
+	$(".rectangle").click(function() {
+		$("h1").css("color", "red")
+	});
+
 
 
 
